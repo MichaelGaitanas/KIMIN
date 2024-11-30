6f8d56e66e09427253c7c16b1570b05b37f62c43
Michael <michaelgaitanas@gmail.com>
2024-11-30 23:15:40 +0200

    Update polyhedron.h

--- source/polyhedron.h
+++ source/polyhedron.h
@@ -14,15 +14,18 @@ class polyhedron
 {
 private:
     bool norms_exist;
+    bool vol_exists;
     dmatnx3 verts;
     umatnx3 faces;
     dmatnx3 norms;
+    double vol; //Polyhedron's total volume.
 
 public:
     //Load the .obj file assuming it has the classical form 'v x y z' and 'f i j k'.
     void load_obj_file(const char *path)
     {
         norms_exist = false;
+        vol_exists = false;
         verts.clear();
         faces.clear();
 
@@ -77,6 +80,9 @@ public:
     //Polyhderon's volume. This function is essential for the inertial integrals evaluation.
     double get_vol()
     {
+        if (vol_exist)
+            return vol;
+
         gen_norms(); //This will only evaluate the norms if norms_exist is false.
 
         double vol = 0.0;
@@ -89,13 +95,15 @@ public:
             double A = 0.5*length(cross(p1-p0, p2-p1));
             vol += d*A/3.0;
         }
+        vol_exists = true;
         return vol;
     }
 
-    //Calculate the center of mass of an arbitrary homogeneous polyhedron.
+    //Calculate the center of mass of an arbitrary homogeneous closed surface polyhedron.
     dvec3 get_com()
     {
         gen_norms(); //This will only evaluate the norms if norms_exist is false.
+        double vol = get_vol();
 
         dvec3 com = {0.0,0.0,0.0};
         for (size_t i = 0; i < faces.size(); ++i)
@@ -123,55 +131,133 @@ public:
             double ny = norms[i][1];
             double nz = norms[i][2];
             double d = x1*nx + y1*ny + z1*nz; //x*nx + y*ny + z*nz - d = 0 (plane equation)
-
-            //Double integral values after the affine transformation.
-            double integx = (3.0*x1 + x21 + x31)/6.0;
-            double integy = (3.0*y1 + y21 + y31)/6.0;
-            double integz = (3.0*z1 + z21 + z31)/6.0;
-
-            //Jacobian determinant that arises due to the affine transformation.
-            double Jacxy = fabs(x21*y31 - x31*y21);
-            double Jacxz = fabs(x21*z31 - x31*z21);
-            double Jacyz = fabs(y21*z31 - y31*z21);
-
-            double area = 0.5*length(cross(dvec3{x2,y2,z2} - dvec3{x1,y1,z1}, dvec3{x3,y3,z3} - dvec3{x2,y2,z2}));
             
-            //comx
             if (fabs(nz) > machine_zero)
-                com[0] += d*Jacxy*integx/fabs(nz);
+            {
+                double Jacxy = fabs(x21*y31 - x31*y21);
+                double coeffnz = d*Jacxy/fabs(nz);
+                com[0] += coeffnz*(3.0*x1 + x21 + x31)/6.0;
+                com[1] += coeffnz*(3.0*y1 + y21 + y31)/6.0;
+                com[2] += coeffnz*(3.0*d - nx*(3.0*x1 + x21 + x31) - ny*(3.0*y1 + y21 + y31))/(6.0*nz);
+            }
             else
+            {
                 if (fabs(ny) > machine_zero)
-                    com[0] += d*Jacxz*integx/fabs(ny);
-                else
-                    com[0] += d*d*area/nx;
-
-            //comy
-            if (fabs(nz) > machine_zero)
-                com[1] += d*Jacxy*integy/fabs(nz);
-            else
-                if (fabs(nx) > machine_zero)
-                    com[1] += d*Jacyz*integy/fabs(nx);
-                else
-                    com[1] += d*d*area/ny;
-
-            //comz
-            if (fabs(ny) > machine_zero)
-                com[2] += d*Jacxz*integz/fabs(ny);
-            else
-                if (fabs(nx) > machine_zero)
-                    com[2] += d*Jacyz*integz/fabs(nx);
-                else
-                    com[2] += d*d*area/nz;
+                {
+                    double Jacxz = fabs(x21*z31 - x31*z21);
+                    double coeffny = d*Jacxz/fabs(ny);
+                    com[0] += coeffny*(3.0*x1 + x21 + x31)/6.0;
+                    com[1] += coeffny*(3.0*d - nx*(3.0*x1 + x21 + x31) - nz*(3.0*z1 + z21 + z31))/(6.0*ny);
+                    com[2] += coeffny*(3.0*z1 + z21 + z31)/6.0;
+                }
+                else //fabs(nx) > machine_zero
+                {
+                    double Jacyz = fabs(y21*z31 - y31*z21);
+                    double coeffnx = d*Jacyz/fabs(nx);
+                    com[0] += coeffnx*(3.0*d - ny*(3.0*y1 + y21 + y31) - nz*(3.0*z1 + z21 + z31))/(6.0*nx);
+                    com[1] += coeffnx*(3.0*y1 + y21 + y31)/6.0;
+                    com[2] += coeffnx*(3.0*z1 + z21 + z31)/6.0;
+                }
+            }
         }
 
-        return com/(4.0*get_vol());
+        return com/(4.0*vol);
+    }
+
+    //Calculate the inertia matrix of an arbitrary homogeneous closed surface polyhedron.
+    dmat3 poly_inertia(const double M)
+    {
+        gen_norms();
+        double vol = get_vol();
+
+        double Jxx = 0.0, Jyy = 0.0, Jzz = 0.0, Jxy = 0.0, Jxz = 0.0, Jyz = 0.0;
+        for (int i = 0; i < faces.size(); ++i)
+        {
+            double x1 = verts[faces[i][0]][0];
+            double y1 = verts[faces[i][0]][1];
+            double z1 = verts[faces[i][0]][2];
+            
+            double x2 = verts[faces[i][1]][0];
+            double y2 = verts[faces[i][1]][1];
+            double z2 = verts[faces[i][1]][2];
+
+            double x3 = verts[faces[i][2]][0];
+            double y3 = verts[faces[i][2]][1];
+            double z3 = verts[faces[i][2]][2];
+
+            double x21 = x2 - x1;
+            double x31 = x3 - x1;
+            double y21 = y2 - y1;
+            double y31 = y3 - y1;
+            double z21 = z2 - z1;
+            double z31 = z3 - z1;
+
+            double nx = norms[i][0];
+            double ny = norms[i][1];
+            double nz = norms[i][2];
+            double d = x1*nx + y1*ny + z1*nz; //x*nx + y*ny + z*nz - d = 0 (plane equation)
+            
+            if (fabs(nz) > machine_zero)
+            {
+                double Jacxy = fabs(x21*y31 - x31*y21);
+                double coeffnz = d*Jacxy/fabs(nz);
+                Jxx +=  coeffnz*(6.0*x1*x1 + x21*x21 + x21*x31 + x31*x31 + 4.0*x1*(x21 + x31))/12.0;
+                Jyy +=  coeffnz*(6.0*y1*y1 + y21*y21 + y21*y31 + y31*y31 + 4.0*y1*(y21 + y31))/12.0;
+                Jzz +=  coeffnz*(6*pow(d,2) + pow(nx,2)*(6*pow(x1,2) + pow(x21,2) + x21*x31 + pow(x31,2) + 4*x1*(x21 + x31)) + pow(ny,2)*(6*pow(y1,2) + pow(y21,2) + y21*y31 + pow(y31,2) + 4*y1*(y21 + y31)) - 4*d*(nx*(3*x1 + x21 + x31) + ny*(3*y1 + y21 + y31)) + nx*ny*(4*x1*(3*y1 + y21 + y31) + x21*(4*y1 + 2*y21 + y31) + x31*(4*y1 + y21 + 2*y31)))/(12.*pow(nz,2));
+                Jxy +=  coeffnz*(4*x1*(3*y1 + y21 + y31) + x21*(4*y1 + 2*y21 + y31) + x31*(4*y1 + y21 + 2*y31))/24.;
+                Jxz += -coeffnz*(-4*d*(3*x1 + x21 + x31) + 2*nx*(6*pow(x1,2) + pow(x21,2) + x21*x31 + pow(x31,2) + 4*x1*(x21 + x31)) + ny*(4*x1*(3*y1 + y21 + y31) + x21*(4*y1 + 2*y21 + y31) + x31*(4*y1 + y21 + 2*y31)))/(24.*nz);
+                Jyz += -coeffnz*(-4*d*(3*y1 + y21 + y31) + 2*ny*(6*pow(y1,2) + pow(y21,2) + y21*y31 + pow(y31,2) + 4*y1*(y21 + y31)) + nx*(4*x1*(3*y1 + y21 + y31) + x21*(4*y1 + 2*y21 + y31) + x31*(4*y1 + y21 + 2*y31)))/(24.*nz);
+            }
+            else
+            {
+                if (fabs(ny) > machine_zero)
+                {
+                    double Jacxz = fabs(x21*z31 - x31*z21);
+                    double coeffny = d*Jacxz/fabs(ny);
+                    Jxx +=  coeffny*(6.0*x1*x1 + x21*x21 + x21*x31 + x31*x31 + 4.0*x1*(x21 + x31))/12.0;
+                    Jyy +=  coeffny*(6*pow(d,2) + pow(nx,2)*(6*pow(x1,2) + pow(x21,2) + x21*x31 + pow(x31,2) + 4*x1*(x21 + x31)) + pow(nz,2)*(6*pow(z1,2) + pow(z21,2) + z21*z31 + pow(z31,2) + 4*z1*(z21 + z31)) - 4*d*(nx*(3*x1 + x21 + x31) + nz*(3*z1 + z21 + z31)) + nx*nz*(4*x1*(3*z1 + z21 + z31) + x21*(4*z1 + 2*z21 + z31) + x31*(4*z1 + z21 + 2*z31)))/(12.*pow(ny,2));
+                    Jzz +=  coeffny*(6.0*z1*z1 + z21*z21 + z21*z31 + z31*z31 + 4.0*z1*(z21 + z31))/12.0;
+                    Jxy += -coeffny*(-4*d*(3*x1 + x21 + x31) + 2*nx*(6*pow(x1,2) + pow(x21,2) + x21*x31 + pow(x31,2) + 4*x1*(x21 + x31)) + nz*(4*x1*(3*z1 + z21 + z31) + x21*(4*z1 + 2*z21 + z31) + x31*(4*z1 + z21 + 2*z31)))/(24.*ny);
+                    Jxz +=  coeffny*(4*x1*(3*z1 + z21 + z31) + x21*(4*z1 + 2*z21 + z31) + x31*(4*z1 + z21 + 2*z31))/24.;
+                    Jyz += -coeffny*(-4*d*(3*z1 + z21 + z31) + 2*nz*(6*pow(z1,2) + pow(z21,2) + z21*z31 + pow(z31,2) + 4*z1*(z21 + z31)) + nx*(4*x1*(3*z1 + z21 + z31) + x21*(4*z1 + 2*z21 + z31) + x31*(4*z1 + z21 + 2*z31)))/(24.*ny);
+                }
+                else //fabs(nx) > machine_zero
+                {
+                    double Jacyz = fabs(y21*z31 - y31*z21);
+                    double coeffnx = d*Jacyz/fabs(nx);
+                    Jxx +=  coeffnx*(6*pow(d,2) + pow(ny,2)*(6*pow(y1,2) + pow(y21,2) + y21*y31 + pow(y31,2) + 4*y1*(y21 + y31)) + pow(nz,2)*(6*pow(z1,2) + pow(z21,2) + z21*z31 + pow(z31,2) + 4*z1*(z21 + z31)) - 4*d*(ny*(3*y1 + y21 + y31) + nz*(3*z1 + z21 + z31)) + ny*nz*(4*y1*(3*z1 + z21 + z31) + y21*(4*z1 + 2*z21 + z31) + y31*(4*z1 + z21 + 2*z31)))/(12.*pow(nx,2));
+                    Jyy +=  coeffnx*(6.0*y1*y1 + y21*y21 + y21*y31 + y31*y31 + 4.0*y1*(y21 + y31))/12.0;
+                    Jzz +=  coeffnx*(6.0*z1*z1 + z21*z21 + z21*z31 + z31*z31 + 4.0*z1*(z21 + z31))/12.0;
+                    Jxy += -coeffnx*(-4*d*(3*y1 + y21 + y31) + 2*ny*(6*pow(y1,2) + pow(y21,2) + y21*y31 + pow(y31,2) + 4*y1*(y21 + y31)) + nz*(4*y1*(3*z1 + z21 + z31) + y21*(4*z1 + 2*z21 + z31) + y31*(4*z1 + z21 + 2*z31)))/(24.*nx);
+                    Jxz += -coeffnx*(-4*d*(3*z1 + z21 + z31) + 2*nz*(6*pow(z1,2) + pow(z21,2) + z21*z31 + pow(z31,2) + 4*z1*(z21 + z31)) + ny*(4*y1*(3*z1 + z21 + z31) + y21*(4*z1 + 2*z21 + z31) + y31*(4*z1 + z21 + 2*z31)))/(24.*nx);
+                    Jyz +=  coeffnx*(4*y1*(3*z1 + z21 + z31) + y21*(4*z1 + 2*z21 + z31) + y31*(4*z1 + z21 + 2*z31))/24.;
+                }
+            }
+        }
+
+        double ord2_coeff = M/(5.0*vol);
+        Jxx *= ord2_coeff;
+        Jyy *= ord2_coeff;
+        Jzz *= ord2_coeff;
+        Jxy *= ord2_coeff;
+        Jxz *= ord2_coeff;
+        Jyz *= ord2_coeff;
+
+        return {{{Jyy + Jzz,    Jxy,       Jxz   },
+                 {   Jxy,    Jxx + Jzz,    Jyz   },
+                 {   Jxz,       Jyz,    Jxx + Jyy}}};
     }
 
     //Shift the center of mass of the polyhedron, so that it coincides with O(0,0,0).
-    void correct_com(const dvec3 &com)
+    void eliminate_com_offset(const dvec3 &com)
     {
         for (size_t i = 0; i < verts.size(); ++i)
             verts[i] = verts[i] - com;
+    }
+
+    void align_principal_axes_to_basis()
+    {
+        //To be added.
     }
 
     //Farthest vertex distance with respect to the local coordinate system.
